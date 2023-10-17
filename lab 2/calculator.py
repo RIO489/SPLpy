991ee170fab66de3db9b2e652ded31eccf139920
RIO489 <oleksandr.hamaiunov.IT.2021@lpnu.ua>
2023-10-18 00:23:48 +0300

    lab 3

--- lab 2/calculator.py
+++ lab 2/calculator.py
@@ -1,6 +1,5 @@
-from cmath import sqrt
-from exception import CalculationException, IncorrectOperatorException
 from data import results
+from exception import CalculationException, IncorrectOperatorException
 from user import User
 
 class Calculator:
@@ -93,88 +92,3 @@ class Calculator:
             confirm = input('Do you want to use this number as num1? Y N: ')
             if(confirm == 'Y' or confirm == "y"):
                 return self.results[i]
-
-#main calculations:
-
-
-
-#     if name == '':
-#         name = input('Enter your name: ')
-#         user = User(name)
-#         setting = input('Do you wish to change user setting? Y N: ')
-#         if(setting == 'Y' or setting == "y"):
-#                 user.roundto = int(input('How many number after dot should be visible? (1-9): '))
-
-#     if len(results):
-#         write = input('Do you with to use results? Y N: ')
-#         if write == 'Y' or write == 'y':
-#             num1=useResult()
-#         else:
-#             num1 = float(input('Enter a first number: '))
-#     else:
-#         num1 = float(input('Enter a first number: '))
-#     num2 = float(input('Enter a second number: '))
-#     operator = input('Enter a operator(+,-,*,/,^,√,%) : ')
-
-#     match operator:
-#         case('+'):
-#             result = num1+num2
-#             print(round(result,user.roundto))
-#             writeIn(result)
-#             save(num1,num2,operator,result)
-#         case('-'):
-#             result = num1 - num2
-#             print(round(result,user.roundto))
-#             writeIn(result)
-#             save(num1,num2,operator,result)
-#         case('*'):
-#             result = num1*num2
-#             print(round(result,user.roundto))
-#             writeIn(result)
-#             save(num1,num2,operator,result)
-#         case('/'):
-#             if(num2 == 0):
-#                 raise CalculationException()
-#                 print('You can`t divide by 0')
-#             else:
-#                 result = num1/num2
-#                 print(round(result,user.roundto))
-#                 writeIn(result)
-#                 save(num1,num2,operator,result)
-#         case('^'):
-#             result = pow(num1,num2)
-#             print(round(result,user.roundto))
-#             writeIn(result)
-#             save(num1,num2,operator,result)
-#         case('√'):
-#                 count = 0
-#                 multiplier = num2
-#                 while(num1 >= num2):
-#                     num2 *= multiplier
-#                     count+=1
-#                 result = num1/num2+count
-#                 print(round(result,user.roundto))
-#                 writeIn(result)
-#                 save(num1,multiplier,operator,result)
-#         case('%'):
-#             if(num2 == 0):
-#                 print('You can`t divide by 0')
-#             else:
-#                 result = num1%num2
-#                 print(round(result,user.roundto))
-#                 writeIn(result)
-#                 save(num1,num2,operator,result)
-#         case _:
-#             print('smth went wrong,please check your inputs')
-
-
-# #function that allow user to view their calculation history
-#     viewHistory = input('Do you want to see your history? Y N: ')
-#     if viewHistory == 'Y' or viewHistory == "y":
-#         for i in range (0, len(user.memory)): 
-#             print(user.memory[i])
-            
-
-#     answer = input('Do you want to make another calculation? Y N: ')
-#     if answer == 'N' or answer == "n":
-#             exit()
--- lab 3/main.py
+++ lab 3/main.py
@@ -1,33 +1,57 @@
 import art
-import pyfiglet
+import sys
+from termcolor import colored
+
 
 #data.py
 user_font = "standart"
 user_art = ""
 memory = []
-
+colors = ["red", "green", "yellow", "blue", "magenta", "cyan", "white"]
+user_char = ""
+replacement_dict = None
 
 def save_art(art):
     memory.append(art)
+
+def printArt(art):
+    sys.stdout.write(art)
+
+def changeChr(text,replacement_dict):
+    for old_char, new_char in replacement_dict.items():
+        text = text.replace(old_char, new_char)
+    return text
+
+def draw_art(text,font,char):
+        return art.text2art(text,font)
+
 
 #Main calculations
 while(1):
     print("1) Enter text")
     print("2) Change font")
     print("3) Print arts")
-    print("4) Quit")
+    print("4) Change color")
+    print("5) Quit")
 
     choice = input("Enter choice: ")
     
     match(choice):
         case("1"):
+            changeChr = input("Do you want use different chr? Y N :")
+            if(changeChr == 'Y'or changeChr == 'y'):
+                user_char = input("Enter the characters you want to use for ASCII art: ")
+                replacement_dict = {'#': user_char[0]}
             text = input("Write text: ")
-            user_art = art.text2art(text,user_font)
+            user_art = draw_art(text,user_font,user_char)
+            if(replacement_dict != None):
+                for old_char, new_char in replacement_dict.items():
+                    user_art = user_art.replace(old_char, new_char)
             print(user_art)
             save = input("Save the art? Y N :")
             if(save == "Y" or save == "y"):
-                art.tsave(user_art,user_font)
-
+                art.tsave(user_art,user_font,filename="test")
+                
         case("2"):
             art.font_list()  
             user_font = input("Choose font: ")
@@ -37,4 +61,8 @@ while(1):
             print(memory)
 
         case("4"):
+            for color in colors:
+                printArt(colored(art.text2art(text="test",font=user_font),color))
+
+        case("5"):
             break
